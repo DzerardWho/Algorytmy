36e5f9acd50ef588fa8dcceaf32ff18ce34ce89f
angroo <anna.n.grochal@gmail.com>
2020-06-12 05:45:16 +0200

    Fit function - critical path method

--- Genetic/decisionTree.py
+++ Genetic/decisionTree.py
@@ -222,16 +222,52 @@ class DecisionTree:
 
         return self, other
 
+    def get_max_path_from_node(self, start_node: Node, proc_inst) -> float:
+        _n = [node for node in self.tasks_graph.nodes if self.embryo.processData[node.label].proc == proc_inst]
+        _leafs = deepcopy(_n)
+        for node in _n:
+            for child in node.children:
+                if self.embryo.processData[child.label].proc == proc_inst:
+                    try:
+                        _leafs.remove(node)
+                    except ValueError:
+                        pass
+
+        all_paths = [[start_node]]
+        for _nod in self.tasks_graph.nodes:
+            for child in _nod.children:
+                for path in all_paths:
+                    if self.tasks_graph.nodes[path[-1].label] in child.parents:
+                        new_path = path[:]
+                        new_path.append(child)
+                        all_paths.append(new_path)
+
+        all_paths = [_p for _p in all_paths if _p[-1] in _leafs]
+        all_paths = [ele for ind, ele in enumerate(all_paths) if ele not in all_paths[:ind]]
+        _max_path_value = 0
+        for path in all_paths:
+            _p_value = 0
+            for n in path:
+                print(n)
+                _p_value += 0
+            if _p_value > _max_path_value:
+                _max_path_value = _p_value
+
+        return _max_path_value
+
     def get_fit_value(self, c: int, t: int) -> float:
-        # TODO extract capcity for channel
+        # TODO extract capacity for channel
         capacity = 1
         _cost = 0
         _time = 0
 
         #     add costs of all used universal proc resources
         for p in self.procInstances:
-            if p[0].proc.universal:
-                _cost += len(p) * p[0].proc.cost
+            try:
+                if p[0].proc.universal:
+                    _cost += len(p) * p[0].proc.cost
+            except IndexError:
+                pass
         # add costs of execution tasks on proc resources
         for i, task in enumerate(self.embryo.processData):
             _cost += task.proc.proc.costs[i]
@@ -248,13 +284,15 @@ class DecisionTree:
         self.embryo.processData[0].proc.time_remaining = self.embryo.processData[0].proc.proc.times[0]
         finished_tasks = []
         while len(finished_tasks) != len(self.tasks_graph.nodes):
-            for a, _parent in available_tasks:
-                # TODO CPM
+            _tasks_ = [(a[0], a[1], self.get_max_path_from_node(a[0], self.embryo.processData[a[0].label].proc))
+                               for a in available_tasks]
+            _tasks_to_start = sorted(_tasks_, key=lambda x: x[2])
+            for a, _parent, _maxpath in _tasks_to_start:
                 if not self.embryo.processData[a.label].proc.time_remaining:
                     ongoing_tasks.append(a)
                     _t = self.embryo.processData[a.label].proc.proc.times[a.label]
-                    if self.embryo.processData[a.label].proc != self.embryo.processData[o.label].proc:
-                        _t += np.ceil(a.parents[o] / capacity)
+                    if self.embryo.processData[a.label].proc != self.embryo.processData[_parent.label].proc:
+                        _t += np.ceil(a.parents[_parent] / capacity)
                     self.embryo.processData[a.label].proc.time_remaining = _t
 
             available_tasks = [a for a in available_tasks if a not in ongoing_tasks]
